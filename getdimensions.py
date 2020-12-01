25875207ca77d13c7f9327c4cb69b4698e2503ea
JFGrobusch <61617930+JFGrobusch@users.noreply.github.com>
2020-12-01 16:46:10 +0100

    Add files via upload

---
+++ getdimensions.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  1 15:22:01 2020
+
+
+
+Import into main document via :
+    
+        import getradius from getdimensions
+
+@author: jangrobusch
+"""
+
+from math import pi
+import numpy as np
+
+def volume(r, maxheight):
+    V = pi*r*(maxheight*r-(2/3)*r**2)
+    return(V)
+
+
+def getdimensions(tank, targetvolume, maxheight):
+    stepsize = 0.1
+    h = maxheight
+    Vdiffs = []
+    rlist = []
+    for i in np.arange(0, 0.5*h, stepsize):
+        rlist.append(i)
+        Vdiffs.append(abs(volume(i, h)-targetvolume))   
+    tank.radius = rlist[Vdiffs.index(min(Vdiffs))]
+    tank.height = h - 2*tank.radius
+
+    return(tank)
+
+class tanky:
+    def __init__(self,):
+        self.radius = 0.
+        self.height = 0.
+
+maxheight = 1.7E3
+V = 3E8
+
+tank = tanky()
+tank = getdimensions(tank, V, maxheight)
+print(tank.radius, tank.height)
+   
+
+    
+    
+    
+
+
+        
