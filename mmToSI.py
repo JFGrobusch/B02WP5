0dc9981878a9c7fa6af1d765c57f8602549187b1
JFGrobusch <61617930+JFGrobusch@users.noreply.github.com>
2020-12-03 15:30:15 +0100

    Update mmToSI.py

--- mmToSI.py
+++ mmToSI.py
@@ -7,10 +7,10 @@ Created on Thu Dec  3 15:20:15 2020
 
 
 def toSI(tank):
-    tank.pressure = tank.pressure/(1000**2) # N/mm^2
-    tank.height = tank.height*1000 #L in mm
-    tank.radius = tank.radius*1000 #r_in in mm
-    tank.thickness = tank.thickness*1000 #t in mm
+    tank.pressure = tank.pressure/(1000**2) # N/m^2
+    tank.height = tank.height*1000 #L in m
+    tank.radius = tank.radius*1000 #r_in in m
+    tank.thickness = tank.thickness*1000 #t in m
     
     return(tank)
 
