1d14ba3b598fee5428866e5603b895d086d2dec8
Tod2133 <75317145+Tod2133@users.noreply.github.com>
2020-12-03 14:22:27 +0100

    Edited terminology

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from Get_Volume import *
 class Fuel_Tank():
     def __init__(self, species):
         self.species = species
-        self.safety_margin = 1.5 #-
+        self.safety_factor = 1.5 #-
         self.pressure = 80 # N/mm^2
         self.height = 0 #L in mm
         self.radius = 415 #r_in in mm
@@ -13,7 +13,8 @@ class Fuel_Tank():
         self.MS_pressure = MS_pressure
         self.MS_euler = MS_euler
         self.MS_shell = MS_shell
-        self.MS = self.MS_pressure + self.MS_euler + self.MS_shell
+        self.MS_total = self.MS_pressure + self.MS_euler + self.MS_shell
+        self.MS = [MS_pressure,MS_euler,MS_shell]
 
 class Margin_of_safety():
     def __init__(self, name, value):
@@ -29,7 +30,7 @@ class Material():
 max_height = 891 #L+2r in mm
 fuel_mass = 480 #kg
 
-number_species = 10
+number_species = 20
 total_tanks = 100
 per_species = total_tanks / number_species
 total_generations = 10
