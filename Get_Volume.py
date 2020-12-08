1a9acd040e2cf5d905077d73f81b944e847de981
Tod2133 <75317145+Tod2133@users.noreply.github.com>
2020-12-08 14:34:06 +0100

    Update Get_Volume.py

--- Get_Volume.py
+++ Get_Volume.py
@@ -1,7 +1,7 @@
 def get_volume(pressure, fuel_mass):
     pressure = 10*pressure #n/mm^2 -> bar
     pressure -= 70
-    density = ((6*10**-5)*pressure**3) -(0.0034*pressure**2) +(0.077*pressure) +1.0064 #kg/m^3
+    density = ((6*10**-5)*pressure**3) -(0.0034*pressure**2) +(0.077*pressure) +1.0064 #kg/L
     print(density)
     density = density * 1000 # convert to kg/m^3
     volume = fuel_mass / density # m^3
