521a46819b26de401708e43062a5c5e6760aea80
Tod2133 <75317145+Tod2133@users.noreply.github.com>
2020-12-08 14:40:36 +0100

    f strings

--- Get_Volume.py
+++ Get_Volume.py
@@ -1,9 +1,8 @@
 def get_volume(pressure, fuel_mass):
-    pressure = 10*pressure #n/mm^2 -> bar
     pressure -= 70
     density = ((6*10**-5)*pressure**3) -(0.0034*pressure**2) +(0.077*pressure) +1.0064 #kg/L
-    print(density)
     density = density * 1000 # convert to kg/m^3
     volume = fuel_mass / density # m^3
-    volume = volume * 10E9
+    volume = volume * 1E9
+    print(f'Fuel density{density}, tank volume{volume}')
     return volume
--- Main.py
+++ Main.py
@@ -61,11 +61,10 @@ volume = get_volume(tank.pressure, fuel_mass)
 def get_volume(pressure, fuel_mass):
     pressure -= 70
     density = ((6*10**-5)*pressure**3) -(0.0034*pressure**2) +(0.077*pressure) +1.0064 #kg/L
-    print(density)
     density = density * 1000 # convert to kg/m^3
     volume = fuel_mass / density # m^3
     volume = volume * 1E9
-    print(volume)
+    print(f'Fuel density{density}, tank volume{volume}')
     return volume
 
 def pressure(tank,material):
